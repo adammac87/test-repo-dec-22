29b048818a9583f9ace21a2bba81af833ab9d61f
Adam Macpherson <adam.ja.mac@gmail.com>
2023-01-25 18:28:33 +0000

    change

--- src/App.js
+++ src/App.js
@@ -3,9 +3,22 @@ import './App.css';
 
 function App() {
   return (
+<>
+    <p classname="header">Lets make a site</p>
 
-    <p>YOYOYO</p>
+
+
+    <p classname="para">This is gonna be hard to remember things and fugure things out but, every great journey start with
+      a few small steps
+    </p>
+
+    <p classname="para">This is gonna be hard to remember things and fugure things out but, every great journey start with
+      a few small steps
+    </p>
+    </>
   );
+  
+  
 }
 
 export default App;
